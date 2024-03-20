30f8ca1171d9128daea70ef87111fe9256c0e2ae
srid2407 <sridharvasu@bren.ucsb.edu>
2024-03-19 23:55:39 -0700

    Final Assignment

--- R/footprint.R
+++ R/footprint.R
@@ -1,17 +1,15 @@
 
 #' Land Footprint (LF) of a Wind Turbine
-#' 
-#' This function determines the LF of a wind turbine based on turbine configuration, rotor
+#'
+#' This function determines the Land Footprint of a wind turbine based on turbine configuration, rotor
 #' diameter and rated capacity of the turbine
-#' @param rd rotor diameter (m) 
+#' @param rd rotor diameter (m)
 #' @param pwr rated capacity of the turbine (MW)
 #' @param nt total number of turbines default = 25
 #' @param cf capacity factory of wind turbine default = 0.2
 #' @param y Lifetime of plant (year) default = 20
 #' @return land footprint (m2/GWh)
-#' 
-#' #function definition
-#' 
+
 
 land_footprint = function(rd, pwr, nt=25, cf=0.2, y=20)
 {
---
+++ R/wind.R
@@ -0,0 +1,13 @@
+#' Wind Turbine Data
+#'
+#' Random Data Generated for rotor diameter and
+#' rated capacity of Wind Turbine
+#'
+#' @format A data frame with 4 rows and 3 variables
+#' \itemize{
+#' \item turbine_id
+#' \item rotor_d
+#' \item rated_pwr }
+#'
+#' @source C:/Users/Sridhar Iyengar/Documents/UCSB/Class/W24/ESM 262/Assignments/ESM262Final/wind.csv
+#' "wind"
